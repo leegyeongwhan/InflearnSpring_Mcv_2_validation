4dabeb50b4dbf8f81165f3ba209df5a4975adf51
leegyeongwhan <leekhy02@naver.com>
2022-11-03 13:01:12 +0900

    feat :  veanvalidation apiController requestbody 와 modelAttribute 의차이

---
+++ src/main/java/hello/itemservice/web/validation/form/ItemSaveForm.java
@@ -0,0 +1,27 @@
+package hello.itemservice.web.validation.form;
+
+
+import hello.itemservice.domain.item.SaveCheck;
+import hello.itemservice.domain.item.UpdateCheck;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class ItemSaveForm {
+
+    //id는필요없음 등록form에서
+    @NotBlank
+    private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
+    private Integer price;
+
+    @NotNull
+    @Max(value = 9999)
+    private Integer quantity;
+}
